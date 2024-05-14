9959c89a894492a31eb57e7cfe13daf5d10b1212
yujeong-world <kyjii082@gmail.com>
2024-05-14 15:27:49 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/bigs_be_test/BigsBeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigs_be_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigsBeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigsBeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bigs_be_test/BigsBeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigs_be_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigsBeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
